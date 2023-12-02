d0b46cb04cd265ef1f7b3c2dabd6495c3f6a9838
Jerry Arhawho <jerryarhawho@gmail.com>
2023-12-02 23:17:56 +0000

    First commit

---
+++ src/main/java/com/jayklef/contentmanager/model/Status.java
@@ -0,0 +1,8 @@
+package com.jayklef.contentmanager.model;
+
+public enum Status {
+    IDEA,
+    IN_PROGRESS,
+    COMPLETED,
+    PUBLISHED
+}
---
+++ src/main/java/com/jayklef/contentmanager/model/Type.java
@@ -0,0 +1,8 @@
+package com.jayklef.contentmanager.model;
+
+public enum Type {
+    ARTICLE,
+    VIDEO,
+    COURSE,
+    CONFERENCE_TALK
+}
